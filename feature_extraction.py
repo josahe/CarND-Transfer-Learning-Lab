b17131e61e046944bc79e6b6febbae7d320714b8
josahe <josahe@users.noreply.github.com>
2018-01-11 22:45:44 +0000

    implemented TODOs

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,8 @@
 import pickle
 import tensorflow as tf
-# TODO: import Keras layers you need here
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,6 +10,9 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+
+flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 256, "The batch size.")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -41,13 +46,37 @@ def main(_):
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
-    # TODO: define your model and hyperparams here
-    # make sure to adjust the number of classes based on
-    # the dataset
-    # 10 for cifar10
-    # 43 for traffic
+    # Define model and hyperparams
+    if FLAGS.training_file.split('_')[1] == 'cifar10':
+        num_classes = 10
+    elif FLAGS.training_file.split('_')[1] == 'traffic':
+        num_classes = 43
+    else:
+        raise ValueError("couldn't infer number of classes from file name - check code")
 
-    # TODO: train your model here
+    # This returns a tensor
+    input_shape = X_train.shape[1:]
+    x = Input(shape=input_shape)
+
+    # a layer instance is callable on a tensor, and returns a tensor
+    y = Flatten()(x)
+    
+    # output_shape = (None, num_classes)
+    # Using softmax, so output will be probability distribution with num_classes many column matrices
+    y = Dense(num_classes, activation='softmax')(y) # output_shape = (None, num_classes)
+    
+    model = Model(x, y)
+
+    # y_train is a sparse representation (ie each example is a single number refering to the index of
+    # the correct class). therefore, can use sparse loss algorithm to take care of transformation
+    # of sparse representation to one-hot vector so that can perform cross entropy between softmax
+    # output and one-hot labels
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+    # Train model
+    model.fit(X_train, y_train, batch_size=FLAGS.batch_size, nb_epoch=FLAGS.epochs,
+              validation_data=(X_val, y_val), shuffle=True)  # starts training
 
 
 # parses flags and calls the `main` function above
